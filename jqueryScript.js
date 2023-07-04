dca56c2828c094adf413dd25c783d3248d5b552c
raphael <raphaelazambuja5@gmail.com>
2023-07-04 11:07:33 -0300

    ajustes no drag and drop para mobile

--- jqueryScript.js
+++ jqueryScript.js
@@ -1,50 +1,61 @@
 var participant = [];
 
-$("#list").sortable({
-  connectWith: "#list",
-  update: updateParticipantList,
-  sortupdate: updateParticipantOrder
-});
+sortable_list()
 
 $("#button").click(() => {
   let participant_datas = $("#name").val() + " " + $("#office").val() + " " + $("#entity").val() + " " + $("#local").val();
 
   participant.push(participant_datas);
 
-  $('<li>', {
-    text: participant[participant.length - 1]
-  }).appendTo('#list');
+  var listItem = $('<li>', {
+    id: 'item-list-' + (participant.length - 1),
+    text: participant_datas
+  });
 
-  show_message()
+  var button = $('<button>', {
+    text: 'Clique Aqui',
+    class: 'btn',
+    click: function() {
+      sortable_list();
+    }
+  });
+
+  button.appendTo(listItem);
+  listItem.appendTo('#list');
+
+  show_message();
 });
 
-function updateParticipantList() 
-{
+function updateParticipantList() {
   var participantOrder = [];
-  $("#list").find('li').each(function() 
-  {
+  $("#list").find('li').each(function() {
     participantOrder.push($(this).text());
   });
 
   console.log("Ordem da lista salva:", participantOrder);
 }
 
-function updateParticipantOrder() 
-{
+function updateParticipantOrder() {
   participant = [];
-  $("#list").find('li').each(function() 
-  {
+  $("#list").find('li').each(function() {
     participant.push($(this).text());
   });
 }
 
-function show_message(){
-  if (participant.length == 0) 
-  {
-  $("p").show();;  
+function show_message() {
+  if (participant.length == 0) {
+    $("p").show();
+  } else {
+    $("p").hide();
   }
-  else
-  {
-  $("p").hide();
-  }
+}
+
+
+function sortable_list() {
+  $("#list").sortable({
+    connectWith: "#list",
+    update: updateParticipantList,
+    sortupdate: updateParticipantOrder
+  });
+  
 }
