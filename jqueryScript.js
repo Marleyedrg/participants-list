1ccb16983f5f07e20314dc8df821c8d241682f71
raphael <raphaelazambuja5@gmail.com>
2023-07-05 14:47:10 -0300

    subindo codigo para testes|eric mobile jspuro

--- jqueryScript.js
+++ jqueryScript.js
@@ -1,6 +1,6 @@
 var participant = [];
 
-sortable_list()
+sortable_list();
 
 $("#button").click(() => {
   let participant_datas = $("#name").val() + " " + $("#office").val() + " " + $("#entity").val() + " " + $("#local").val();
@@ -13,7 +13,7 @@ $("#button").click(() => {
   });
 
   var button = $('<button>', {
-    text: 'Clique Aqui',
+    text: '+',
     class: 'btn',
     focus: function() {
       sortable_list();
@@ -25,7 +25,7 @@ $("#button").click(() => {
 
   show_message();
 });
-
+//   ;)
 // functions ---------------------------
 
 function updateParticipantList() {
@@ -35,6 +35,9 @@ function updateParticipantList() {
   });
 
   console.log("Ordem da lista salva:", participantOrder);
+
+  // Vibra o celular
+  vibrateDevice();
 }
 
 function updateParticipantOrder() {
@@ -52,12 +55,44 @@ function show_message() {
   }
 }
 
-
 function sortable_list() {
   $("#list").sortable({
     connectWith: "#list",
     update: updateParticipantList,
     sortupdate: updateParticipantOrder
   });
-  
+
+  // Adicione o comportamento de arrastar e soltar usando eventos de toque
+  $("#list li")
+    .on("touchstart", function(event) {
+      event.preventDefault();
+      $(this).addClass("dragging");
+    })
+    .on("touchmove", function(event) {
+      event.preventDefault();
+      const touch = event.touches[0];
+      const touchX = touch.pageX;
+      const touchY = touch.pageY;
+      $(this).css({
+        position: "absolute",
+        left: touchX + "px",
+        top: touchY + "px"
+      });
+    })
+    .on("touchend", function(event) {
+      $(this).removeClass("dragging");
+      $(this).css({
+        position: "static",
+        left: "auto",
+        top: "auto"
+      });
+    });
+}
+
+// Função para vibrar o celular
+function vibrateDevice() {
+  // Verifica se o navegador suporta a API de vibração
+  if ("vibrate" in navigator) {
+    navigator.vibrate(100); // Vibra por 100 milissegundos
+  }
 }
